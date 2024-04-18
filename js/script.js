c5c68743e318c9b18b7b7cc655cdf8a95eb0335a
Gabriel Castro <137096031+chatfly@users.noreply.github.com>
2024-04-18 18:41:02 -0300

    updata meu

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ function confirmarnickname(){
     document.getElementById("buttoncoletarmadeira").style.display = "block"
     document.getElementById("selectmachado").style.display = "block"
     document.getElementById("arvoreprincipal").style.display = "block"
+    document.getElementById("menuselecionar").style.display = "block"
 
     document.getElementById("divnome").remove()
 }
